2f7febdbc83cd70a6a4b8970ee181e09b270c63d
liz-s <liz.s@kakaocorp.com>
2025-09-19 16:15:28 +0900

    HashRouter로 수정

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ProjectProvider from './contexts/project/project.context';
 // root.js
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // require("dotenv").config();
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ProjectProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </ProjectProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 reportWebVitals();
